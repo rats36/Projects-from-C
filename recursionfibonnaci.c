4329e3850400f51d33aab9eedac1fcbe9399dec4
Abeeku <32397266+rats36@users.noreply.github.com>
2020-12-22 17:18:01 +0100

    Add files via upload
    Recursive Fibonacci Sequence in c

---
+++ recursionfibonnaci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+    if(n < 2)
+    {
+        return n;
+    }
+    else
+    {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+int main()
+{
+    int x;
+    printf("Geben Sie eine Zahl: ");
+    scanf("%d", &x);
+
+    printf(" Ihre  %dte Fibonnaci Zahl ist:%d ", x,fib(x));
+}
